2eed696e22d820c891c23b85022d5e334a7b2cbe
Onton <bashprog@gmail.com>
2023-02-09 12:57:03 +0600

    First commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+
+root.render(
+    <>
+        <h1>TEST</h1>
+    </>
+)
+
