bcc08abd2d72854d312e89c9e11dc949ff63a972
Onton <bashprog@gmail.com>
2023-02-09 13:39:12 +0600

    Ushel

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import './main.scss';
 import CardContainer from "./containers/CardContainer/CardContainer";
 
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
+//Время 13:38, надо отлучиться сорри на спидран писанины не будет похоже
 
 root.render(
     <>
