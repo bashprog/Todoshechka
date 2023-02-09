ef8468f692b24f694cbdf216055deeeb5d6b17d3
Onton <bashprog@gmail.com>
2023-02-09 14:59:10 +0600

    Displays num's of elements in items array

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,13 +4,19 @@ import './Card.scss';
 
 import AddInput from "./AddInput";
 
+import {useCardStore} from "../../containers/CardContainer/store";
+import Item from "./Item";
+
 const Card: React.FC = () => {
+    const {items} = useCardStore();
+
     return(
         <>
             <div className="container">
                 <div className="flex w100 center">
                     <div className="desk">
                         <AddInput/>
+                        {items.length ? items.map(el => (<Item/>)) : null}
                     </div>
                 </div>
             </div>
---
+++ src/components/Card/Item.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Item: React.FC = () => {
+    return(
+        <h1>ITEM</h1>
+    )
+};
+
+export default Item;
